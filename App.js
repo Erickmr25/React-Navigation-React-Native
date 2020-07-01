a3fe81c691aebbe9ba9f6611c86aec66d654c774
unknown <ericking2501@gmail.com>
2020-07-01 02:01:05 -0600

    v1.0

---
+++ App.js
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+
+export default App;
